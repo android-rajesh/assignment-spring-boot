a0335d4f1f2c5c48f798426b714527c83a411526
Rajesh Pandey <rajesh.pandey@effem.com>
2021-08-23 00:16:06 +0530

    enabling cors for 3000 port

--- src/main/java/assignment/react/web/ApiController.java
+++ src/main/java/assignment/react/web/ApiController.java
@@ -5,16 +5,14 @@ import assignment.react.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ApiController {
     private SalaryService salaryService;
     private EmployeeService employeeService;
